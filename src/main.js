44f1ea77d4230695f42bba522842b349389953dc
Johan Öhrn <johan.ohrn@protonmail.com>
2019-04-08 09:50:59 +0200

    Add space animation interactivity

--- src/main.js
+++ src/main.js
@@ -15,6 +15,15 @@
   var context = canvas.getContext("2d");
   var stars = [];
 
+  var mouseOffsetX = 0;
+  var mouseOffsetY = 0;
+  var scrollOffsetY = 0;
+  var speedOffset = 0;
+  var speedOffsetExpo = 0;
+  var animationOffsetX = 0;
+  var animationOffsetY = 0;
+  var animationSpeedOffset = 0;
+
   // create stars data
   for (var i = 0; i < STARS; i++) {
     stars[i] = {
@@ -36,13 +45,17 @@
     HALF_WIDTH = WIDTH / 2;
     HALF_HEIGHT = HEIGHT / 2;
 
-    // set canvas size
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
+    // animation offset
+    animationOffsetX += (mouseOffsetX - animationOffsetX) * 0.1;
+    animationOffsetY += (mouseOffsetY - scrollOffsetY - animationOffsetY) * 0.1;
 
-    // clear canvas of stars
-    context.fillStyle = "rgb(0,0,0)";
-    context.clearRect(0, 0, WIDTH, HEIGHT);
+    // speed offset animation
+    animationSpeedOffset +=
+      (speedOffset - animationSpeedOffset) * speedOffsetExpo;
+
+    // render space
+    context.fillStyle = "rgba(0,0,0,1)";
+    context.fillRect(0, 0, WIDTH, HEIGHT);
 
     // render each star
     for (var i = 0; i < STARS; i++) {
@@ -51,7 +64,7 @@
       var z = stars[i].z;
 
       // move star
-      z -= SPEED;
+      z -= SPEED + animationSpeedOffset;
 
       // reset star if it passes the fov
       if (z < -FOV) z += DEPTH;
@@ -61,18 +74,64 @@
 
       // calculate position and scale
       var scale = FOV / (FOV + z);
-      var x2d = x * scale + HALF_WIDTH;
-      var y2d = y * scale + HALF_HEIGHT;
+      var x2d = x * scale + HALF_WIDTH + animationOffsetX;
+      var y2d = y * scale + HALF_HEIGHT + animationOffsetY;
 
       // render to canvas context
-      context.lineWidth = scale;
-      context.strokeStyle = "rgb(255,255,255)";
       context.beginPath();
+      context.fillStyle = "#fff";
       context.moveTo(x2d, y2d);
-      context.lineTo(x2d + scale, y2d);
-      context.stroke();
+      context.arc(x2d, y2d, 0.5 * scale, 0, Math.PI * 2, true);
+      context.fill();
     }
   };
 
+  var resize = function() {
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+  };
+
+  var moveSpace = function(x, y) {
+    mouseOffsetX = (HALF_WIDTH - x) * 0.1;
+    mouseOffsetY = (HALF_HEIGHT - y) * 0.1;
+  };
+
+  var panSpace = function(y) {
+    scrollOffsetY = y * -0.25;
+  };
+
+  window.addEventListener("resize", function() {
+    resize();
+  });
+
+  window.addEventListener("mousemove", function(event) {
+    moveSpace(event.clientX, event.clientY);
+  });
+
+  window.addEventListener("touchstart", function(event) {
+    var touch = event.touches[0];
+    moveSpace(touch.clientX, touch.clientY);
+  });
+
+  window.addEventListener("touchmove", function(event) {
+    var touch = event.touches[0];
+    moveSpace(touch.clientX, touch.clientY);
+  });
+
+  window.addEventListener("scroll", function() {
+    panSpace(window.pageYOffset);
+  });
+
+  canvas.parentElement.addEventListener("mousedown", function() {
+    speedOffsetExpo = 0.001;
+    speedOffset = 20;
+  });
+
+  canvas.parentElement.addEventListener("mouseup", function() {
+    speedOffsetExpo = 0.1;
+    speedOffset = 0;
+  });
+
   render();
+  resize();
 })();
