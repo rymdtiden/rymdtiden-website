91e9388d4f3db9e7e684f85fd9d015a6f5bcfd49
Johan Öhrn <johan.ohrn@protonmail.com>
2019-04-11 10:29:00 +0200

    Add dev server with live reload

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,43 @@ var gulp = require("gulp");
 var fileInline = require("gulp-file-inline");
 var htmlmin = require("gulp-htmlmin");
 var gzip = require("gulp-gzip");
+var connect = require("gulp-connect");
+
+var outputFolder = "dist";
 
 function build() {
   return gulp
     .src("src/index.html")
     .pipe(fileInline())
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest(outputFolder))
     .pipe(gzip())
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest(outputFolder));
 }
 
-gulp.task("default", build);
+function watch(done) {
+  gulp.watch(
+    ["src/*.html", "src/*.css", "src/*.js"],
+    gulp.series(build, reload)
+  );
+  done();
+}
 
-gulp.task("watch", function() {
-  gulp.watch("src/*.html", build);
-  gulp.watch("src/*.css", build);
-  gulp.watch("src/*.js", build);
-});
+function server(done) {
+  connect.server(
+    {
+      root: outputFolder,
+      livereload: true
+    },
+    function() {
+      this.server.on("close", done);
+    }
+  );
+}
+
+function reload() {
+  return gulp.src(outputFolder).pipe(connect.reload());
+}
+
+gulp.task("default", gulp.parallel(build, server, watch));
+gulp.task("build", build);
